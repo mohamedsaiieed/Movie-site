f4915ae855e8f75eeccfc9838ad26f82ddeeccbe
saied <modymohamed01157@gmail.com>
2023-08-11 05:52:40 +0300

    first commit

--- src/redux/actions/MovieAction.js
+++ src/redux/actions/MovieAction.js
@@ -12,7 +12,6 @@ export const getMovieSearch = (word) => {
     return async (dispatch) => {
         const res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=8dd94b085b4374ae669f7f33a3c6d712&query=${word}&language=en-US`)
         dispatch({ type: AllMovies, data: res.data.results, pages: res.data.total_pages })
-
     }
 }
 
